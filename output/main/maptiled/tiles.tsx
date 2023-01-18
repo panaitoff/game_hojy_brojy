174dda16f1541c268605eb9f23fe21ff3ffe3993
panaitoff <panaitoff@gmail.com>
2023-01-18 20:37:12 +0300

    ver 2.0

---
+++ output/main/maptiled/tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="tiles" tilewidth="64" tileheight="64" tilecount="39" columns="13">
+ <image source="tiles.png" width="832" height="192"/>
+</tileset>
